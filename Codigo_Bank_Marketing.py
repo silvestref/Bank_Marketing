87091515fb3494602cc2dc20c9acdeaed801a341
Franco Silvestre Urbina <sthepanof32@gmail.com>
2021-08-11 19:07:32 -0500

    Análisis y visualización de datos parte 1

--- Codigo_Bank_Marketing.py
+++ Codigo_Bank_Marketing.py
@@ -53,6 +53,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.experimental import enable_iterative_imputer
 from sklearn.impute import IterativeImputer
 from sklearn.ensemble import RandomForestRegressor
+from matplotlib.ticker import FormatStrFormatter
 
 # El conjunto de datos con el que vamos a tratar almacena características de 11162 personas a los que un banco
 # contacto para ofrecerles el servicio de deposito a plazo fijo, e indica si estos al final decidieron adquirir
@@ -282,22 +283,240 @@ data2 = pd.concat([X, y], axis=1)
 
 data2.columns = nom_cols
 
-
 for col in cols:
     data2[col] = dic[col].inverse_transform(data2[col].astype(int))
 
-# Podemos observar
+# Debido a que las predicciones hechas por los bosques aleatorios se basan en el promedio del resultado de 
+# varios arboles de decision, tendremos algunos datos imputados como decimal en variables que son enteras, como
+# en el caso de "age", es por ello que redondearemos dichos valores decimales en cada variable que solo 
+# contenga valores enteros
 
-fig, ax = plt.subplots(2, 2, figsize=(14,7))
-sns.boxplot(ax=ax[0][0], data= data2[["age", "day", "campaign", "previous"]], palette="Set3")
-sns.boxplot(ax=ax[0][1], data= data2[["balance"]], palette="Pastel1")
-sns.boxplot(ax=ax[1][0], data= data2[["duration"]], palette="Pastel1")
+for col in ["age", "day", "campaign", "previous", "balance", "duration"]:
+    data2[col] = data2[col].round()
+
+# Ahora si podemos graficar para observar el cambio en nuestros datos despues de la imputacion
+
+fig, ax = plt.subplots(1, 3, figsize=(14,7))
+sns.boxplot(ax=ax[0], data= data2[["age", "day", "campaign", "previous"]], palette="Set3")
+sns.boxplot(ax=ax[1], data= data2[["balance"]], palette="Pastel1")
+sns.boxplot(ax=ax[2], data= data2[["duration"]], palette="Pastel1")
 plt.show()
+
+# Del grafico podemos observar que que todas las variables a excepcion de "balance" y "duration" estan libres
+# de outliers.
+
+fig, ax = plt.subplots(1, 2, figsize=(14,7))
+sns.boxplot(ax=ax[0], data= data2[["balance"]], palette="Set3")
+sns.distplot(data2["balance"], ax=ax[1])
+
+fig, ax = plt.subplots(1, 2, figsize=(14,7))
+sns.boxplot(ax=ax[0], data= data2[["duration"]], palette="Set3")
+sns.distplot(data2["duration"], ax=ax[1])
+
+# Analizando las variables que aun tienen presencia de valores atipicos, se ve que la varianza en la distribucion
+# de estos valores ya no es tan extrema como teniamos inicialmente, si no que ahora se distribuyen en un rango
+# menor a 1000 unidades, incluso pudiendose acercar a una distribucion normal.
 
 Q1 = data2.quantile(0.25)
 Q3 = data2.quantile(0.75)
 IQR = Q3 - Q1
 
 ( (data2 < (Q1 - 1.5 * IQR)) | (data2 > (Q3 + 1.5 * IQR)) ).sum() / data2.shape[0] * 100
+
+# A la vez que tambien observamos que estos datos atipicos solo constituyen el 5.6% y 4.1% respectivamente 
+# del total, lo cual es una cifra moderadamente baja. Entonces podemos tomar dos decisiones, elimnarlos o
+# conservarlos como parte de nuestros datos. En esta ocacion, eligire conservarlos ya que pueden contener
+# informacion util para el analisis y para el modelo de clasificacion, ademas que su presencia es relativamente
+# baja con respecto del total y su distancia de los extremos no es tan alarmante ni exagerada.
+
+
+#------------------------------------
+# IDENTIFICACIÓN DE VALORES FALTANTES 
+#------------------------------------
+
+# Observamos cuantos valores faltantes hay en nuestro conjunto de datos
+data2.isnull().sum().sum()
+
+# Debido a que no hay presencia de valores faltantes o nulos, no sera necesario tomar acciones al respecto
+
+
+
+#------------------------------------------------------------------------------------------------------------
+#                                      ANÁLISIS Y VISUALIZACIÓN DE DATOS
+#------------------------------------------------------------------------------------------------------------
+
+# En base a las variables que tenemos disponible empezaremos la sección formulando algunas hipótesis que
+# seran respondidas mediante el proceso de análisis de los datos.
+
+# H1: ¿Es la edad del cliente un factor que propicie la solicitud de un deposito a plazo fijo?
+# H2: ¿Que tipo de trabajos son mas propensos a tener clientes que quieran solicitar un deposito a plazo fijo?
+# H3: ¿Los clientes casados son menos propensos a solicitar un deposito a plazo fijo?
+# H4: ¿El grado de educacion alcanzado por el cliente propicia a la solicitud de un deposito a plazo fijo?
+# H5: ¿Los clientes con mora crediticia en el banco son menos propensos a solicitar un deposito a plazo fijo?
+# H6: ¿Se puede decir que los clientes con mayor dinero en su cuenta bancaria son muy propensos a solicitar un
+# deposito a plazo fijo?
+# H7: ¿Los clientes con un prestamo para vivienda en el banco son menos propensos a solicitar un deposito a plazo fijo?
+# H8: ¿Los clientes con un prestamo personal en el banco son menos propensos a solicitar un deposito a plazo fijo?
+# H9: ¿El medio de comunicacion con el que se contacta con el cliente afecta en la solicitud de un deposito a plazo fijo?
+# H10: ¿Existen dias especificos en los que sea mas probable convencer a un cliente de solicitar un deposito a plazo fijo?
+# H11: ¿Existen meses especificos en los que sea mas probable convencer a un cliente de solicitar un deposito a plazo fijo?
+# H12: ¿Se puede decir que a mayor duracion en tiempo de constacto con el cliente aumentan las posibilidades de
+# que este acepte solicitar un deposito a plazo fijo?
+# H13: ¿Es cierto que mientras mas contactos se tenga con el cliente mayor sera la posibilidad de que este
+# termine aceptando solicitar un deposito a plazo fijo?
+# H14: ¿El numero de contactos realizados en la campaña anterior afecta en la posibilidad de que los clientes
+# soliciten un deposito a plazo fijo?
+# H15: ¿Los clientes que solicitaron un deposito a plazo fijo en la campaña anterior son mas propensos a solicitar
+# el mismo servicio en la campaña actual?
+
+
+# Para comenzar, visualizaremos la distribución de los datos respecto a cada uno de los tres conjuntos de
+# variables que se han identificado: Variables de información del cliente - Variables de informacion bancaria
+# - Variables de campaña. Esta segmentación nos permitirá realizar un análisis mas ordenado e identificar
+# patrones e información util para entender nuestros datos.
+
+
+# VARIABLES DE INFORMACIÓN DEL CLIENTE
+
+fig, ax = plt.subplots(2, 2, figsize=(16, 8))
+plt.subplots_adjust(wspace=0.2, hspace=0.3)
+sns.histplot(data=data2, x="age", kde=True, ax=ax[0,0], color="g")
+ax[0,0].set_title("age")
+ax[0,0].set_xlabel("")
+sns.countplot(data=data2, x="marital", ax=ax[0,1])
+ax[0,1].set_title("marital")
+ax[0,1].set_xlabel("")
+sns.countplot(data=data2, x="education", ax=ax[1,0])
+ax[1,0].set_title("education")
+ax[1,0].set_xlabel("")
+sns.countplot(data=data2, x="contact", ax=ax[1,1])
+ax[1,1].set_title("contact")
+ax[1,1].set_xlabel("")
+fig.suptitle('Distribución de las variables de información del cliente', fontsize=16)
+plt.show()
+
+fig, ax = plt.subplots(1, 1, figsize=(14, 4))
+sns.countplot(data=data2, y="job")
+ax.set_title("job")
+ax.set_ylabel("")
+plt.show()
+
+# Observamos que la mayoria de clientes del banco tienen edades que entran en el rango de los 30 y 40 años, sin
+# embargo, la diferencia entre el numero de clientes que entran en este rango y los que no, no es muy grande.
+# Entonces podemos decir que el banco en su gran mayoria tiene clientes que no sobrepasan la mediana edad.
+
+# Tambien podemos observar que la mayoria de estas personas son casadas y que muy pocas son divorciadas. A la
+# vez que tambien se ve que el tipo de educacion predominante es la secundaria y terciaria, lo cual tiene 
+# sentido ya que en estos niveles de eduacion se aprenden materias que guardan relacion con la economia y la
+# sociedad.
+
+# Se aprecia tambien que el medio de contacto preferido por los clientes es el celular
+
+# Por ultimo, la mayoria de clientes del banco tienen puestos de gerencia, obrero y tecnico, y muy pocos son
+# amas de casa, emprendedores o desempleados.
+
+
+# VARIABLES DE INFORMACIÓN BANCARIA
+
+fig, ax = plt.subplots(2, 2, figsize=(16, 8))
+plt.subplots_adjust(wspace=0.2, hspace=0.3)
+sns.countplot(data=data2, x="default", ax=ax[0,1])
+ax[0,1].set_title("default")
+ax[0,1].set_xlabel("")
+sns.histplot(data=data2, x="balance", kde=True, ax=ax[0,0], color="g")
+ax[0,0].set_title("balance")
+ax[0,0].set_xlabel("")
+sns.countplot(data=data2, x="housing", ax=ax[1,0])
+ax[1,0].set_title("housing")
+ax[1,0].set_xlabel("")
+sns.countplot(data=data2, x="loan", ax=ax[1,1])
+ax[1,1].set_title("loan")
+ax[1,1].set_xlabel("")
+fig.suptitle('Distribución de las variables de información del cliente', fontsize=16)
+plt.show()
+
+# Con respecto a la variable "balance" (saldo del cliente en su cuenta bancaria) observamos que existen muchos
+# clientes que tienen relativamente poco dinero acumulado en sus cuentas, estos valores se encuentran en un
+# rango mayor a 0 y menor a 1000.
+
+# Tambien podemos observar que casi no existen clientes morosos en el banco, esta variable se podria relacionar
+# con "balance" en donde se observa que hay muy pocas personas con saldo negativo en sus cuentas bancarias
+
+# Por otro lado, tenemos que la cantidad de clientes que han solicitado un prestamo para vivienda es muy
+# similar a la cantidad de clientes que no solicitaron dicho prestamo.
+
+# Por ultimo, observamos que la gran mayoria de clientes no han solicitado un prestamo personal, y los que si
+# lo han hecho, debido a que son minoria, podrian relacionarse con la poca presencia de clientes deudores en 
+# la variable "default" y la poca presencia de clientes con saldo negativo en la variable "balance".
+
+
+# VARIABLES DE CAMPAÑA
+fig, ax = plt.subplots(2, 1, figsize=(12, 8))
+plt.subplots_adjust(wspace=0.2, hspace=0.3)
+sns.countplot(data=data2, x="day", ax=ax[0])
+ax[0].set_title("day")
+ax[0].set_xlabel("")
+ax[0].set_xticklabels(range(1,32))
+sns.countplot(data=data2, x="month", ax=ax[1], order=['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug',
+                                                      'sep', 'oct', "nov", "dec"])
+ax[1].set_title("month")
+ax[1].set_xlabel("")
+fig.suptitle('Distribución de las variables de información del cliente', fontsize=16)
+plt.show()
+
+fig, ax = plt.subplots(2, 2, figsize=(16, 8))
+plt.subplots_adjust(wspace=0.2, hspace=0.3)
+sns.histplot(data=data2, x="duration", kde=True, ax=ax[0,0], color="g")
+ax[0,0].set_title("duration")
+ax[0,0].set_xlabel("")
+sns.countplot(data=data2, x="campaign", ax=ax[0,1])
+ax[0,1].set_title("campaign")
+ax[0,1].set_xlabel("")
+sns.countplot(data=data2, x="previous", ax=ax[1,0])
+ax[1,0].set_title("previous")
+ax[1,0].set_xlabel("")
+sns.countplot(data=data2, x="poutcome", ax=ax[1,1])
+ax[1,1].set_title("poutcome")
+ax[1,1].set_xlabel("")
+fig.suptitle('Distribución de las variables de información del cliente', fontsize=16)
+plt.show()
+
+# Observamos que la cantidad de veces con respecto a los dias en los que se contacta al cliente por ultima vez
+# estan distribuidos de forma casi equitativa, en donde solo se observan picos muy bajos en los dias 1, 10, 24
+# y 31 de cada mes, y los picos mas altos son los que se acercan a principio, quincena o final de cada mes. 
+# Esto se debe probablemente a que estos dias son previos al pago que reciben los clientes en su trabajo, por
+# ende, se intenta aprovechar el ingreso de dinero que tienen esos dias para ofrecerles los servicios de deposito
+# a plazo fijo.
+
+# Con respecto al mes en el que se realizo el ultimo contacto, se observa que Mayo es un mes con mucha importancia
+# para realizar contacto con los clientes, esto podria deberse a que los datos que estamos analizando provienen
+# de un banco en Portugal, y dicho pais celebra en este mes el dia del trabajo, por lo tanto se puede suponer
+# que todos los trabajadores en dicho pais reciben un incentivo economico y el banco aprovecha esta situacion
+# para que el dinero recibido por sus clientes se invierta en la empresa.
+
+# Por otro lado, respecto a la duracion del ultimo contacto en segundos, podemos ver que la gran mayoria de estos
+# tuvo una duracion entorno a los 100 y 300 segundos (1.6 y 3.3 minutos respectivamente), lo cual es un tiempo
+# justo para saber la decision final del cliente
+
+# El numero de contactos realizados en esta campaña son en su gran mayoria 1 o 2. Y el numero de contactos 
+# realizados en la anterior campaña esta muy inclinado al 0, por lo tanto se puede deducir que el banco tiene
+# nuevos clientes o que la campaña anterior no fue ejecutada de forma adecuada.
+
+# Por ultimo, respecto a la variable "poutcome" (resultado de la campaña anterior) podemos observar que una
+# inmensa mayoria de clientes estan etiquetados como "unknown" (desconocido), lo cual respalda la suposicion
+# que anteriormente habiamos hecho respecto a que el banco tenia nuevo clientes, debido a que esta variable 
+# guarda relacion con "previous".
+
+
+
+
+
+
+
+
+
+
+
+
 
 
